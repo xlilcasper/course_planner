725cb1200d0b8904217923a7ee2ba69c9bc06b96
Brian J. Cobb <bjcobb@svsu.edu>
2015-11-18 12:12:48 -0500

    worked on figuring out classes to take

--- js/dal/CS.js
+++ js/dal/CS.js
@@ -1,75 +1,89 @@
 /**
  * Created by Brian Cobb on 11/17/2015.
  */
+//Check out http://catalog.svsu.edu/search_advanced.php?cur_cat_oid=24&search_database=Search&search_db=Search&cpage=1&ecpage=1&ppage=1&spage=1&tpage=1&location=3&filter%5Bkeyword%5D=cis311&filter%5Bexact_match%5D=1
+//to see if we can't some how pull this data programmatically.
 degreeInfo["CS"]=new Degree();
 
 //Format is the same. each entry in required is required. if the entry in required is an array, any of those fill that requirment.
-degreeInfo["CS"].required.push(["MATH161"]);
-degreeInfo["CS"].required.push(["MATH300"]);
-degreeInfo["CS"].required.push(["COMM105A"]);
-degreeInfo["CS"].required.push(["ENGL304"]);
-degreeInfo["CS"].required.push(["PHIL205A","PHIL210B"]);
-degreeInfo["CS"].required.push(["CS105","CS107"]);
-degreeInfo["CS"].required.push(["CS116"]);
-degreeInfo["CS"].required.push(["CS146"]);
-degreeInfo["CS"].required.push(["CS216"]);
-degreeInfo["CS"].required.push(["CIS255"]);
-degreeInfo["CS"].required.push(["CIS311"]);
-degreeInfo["CS"].required.push(["CS316"]);
-degreeInfo["CS"].required.push(["CS331"]);
-degreeInfo["CS"].required.push(["CIS355"]);
+degreeInfo["CS"].addRequired(["MATH140"]);
+degreeInfo["CS"].addRequired(["MATH161"]);
+degreeInfo["CS"].addRequired(["MATH223"]);
+degreeInfo["CS"].addRequired(["MATH300"]);
+degreeInfo["CS"].addRequired(["COMM105A"]);
+degreeInfo["CS"].addRequired(["ENGL304"]);
+degreeInfo["CS"].addRequired(["PHIL205A","PHIL210B"]);
+degreeInfo["CS"].addRequired(["CS105","CS107"]);
+degreeInfo["CS"].addRequired(["CS116"]);
+degreeInfo["CS"].addRequired(["CS146"]);
+degreeInfo["CS"].addRequired(["CS216"]);
+degreeInfo["CS"].addRequired(["CS245"]);
+degreeInfo["CS"].addRequired(["CIS255"]);
+degreeInfo["CS"].addRequired(["CIS311"]);
+degreeInfo["CS"].addRequired(["CS316"]);
+degreeInfo["CS"].addRequired(["CS331"]);
+degreeInfo["CS"].addRequired(["CIS355"]);
+
+degreeInfo["CS"].addRequired(["CIS366"]);
+degreeInfo["CS"].addRequired(["CS401"]);
+degreeInfo["CS"].addRequired(["CS411"]);
+degreeInfo["CS"].addRequired(["CS421"]);
+degreeInfo["CS"].addRequired(["CS446"]);
+degreeInfo["CS"].addRequired(["CS451"]);
 
 degreeInfo["CS"].electiveCredit=8;
-degreeInfo["CS"].electives.push(["CS402"]);
-degreeInfo["CS"].electives.push(["CS416"]);
-degreeInfo["CS"].electives.push(["CS422"]);
-degreeInfo["CS"].electives.push(["CS431"]);
-degreeInfo["CS"].electives.push(["CS476"]);
-degreeInfo["CS"].electives.push(["CS482"]);
-degreeInfo["CS"].electives.push(["CS490"]);
-
-//array to hold all our prereq data. Right now this has to be manually built until a good parsing system can be built
+degreeInfo["CS"].addElective(["CS402"]);
+degreeInfo["CS"].addElective(["CS403"]);
+degreeInfo["CS"].addElective(["CS416"]);
+degreeInfo["CS"].addElective(["CS422"]);
+degreeInfo["CS"].addElective(["CS431"]);
+degreeInfo["CS"].addElective(["CS476"]);
+degreeInfo["CS"].addElective(["CS482"]);
+degreeInfo["CS"].addElective(["CS490"]);
 
 //Each add is an or condition. So this must have MATH103 OR MATH120A OR MATH120B ...
-degreeInfo["CS"].addReq("CS116",["MATH103","MATH120A","MATH120B","MATH140","MATH161"]);
+degreeInfo["CS"].addPreReq("CS116",["MATH103","MATH120A","MATH120B"]);
 //Doing a second add makes it an AND condition. So it must also have one of the following courses.
-degreeInfo["CS"].addReq("CS116",["CS105","CS107");
+degreeInfo["CS"].addPreReq("CS116",["CS105","CS107"]);
 
-degreeInfo["CS"].addReq("CS216",["MATH120A","MATH120B","MATH140","MATH161"]);
-degreeInfo["CS"].addReq("CS216",["CS116"]);
+//Add in math prereqs
+degreeInfo["CS"].addPreReq("MATH300",["MATH161"]);
+degreeInfo["CS"].addPreReq("MATH161",["MATH140"]);
+degreeInfo["CS"].addPreReq("MATH140",["MATH120A","MATH120B"]);
 
-degreeInfo["CS"].addReq("CS245",["MATH120B","MATH140","MATH161"]);
-degreeInfo["CS"].addReq("CS245",["CS116"]);
+degreeInfo["CS"].addPreReq("CS216",["MATH120A","MATH120B","MATH140","MATH161"]);
+degreeInfo["CS"].addPreReq("CS216",["CS116"]);
 
-degreeInfo["CS"].addReq("CS316",["CS216"]);
+degreeInfo["CS"].addPreReq("CS245",["MATH120B","MATH140","MATH161"]);
+degreeInfo["CS"].addPreReq("CS245",["CS116"]);
+
+degreeInfo["CS"].addPreReq("CS316",["CS216"]);
 degreeInfo["CS"].addConcurrent("CS316",["MATH300"]);
 
-degreeInfo["CS"].addReq("CS331",["CS216"]);
+degreeInfo["CS"].addPreReq("CS331",["CS216"]);
 
-degreeInfo["CS"].addReq("CS333",["CS216"]);
-degreeInfo["CS"].addReq("CS333",["CJ201"]);
+degreeInfo["CS"].addPreReq("CS333",["CS216"]);
+degreeInfo["CS"].addPreReq("CS333",["CJ201"]);
 
-degreeInfo["CS"].addReq("CS345",["CS216"]);
+degreeInfo["CS"].addPreReq("CS345",["CS216"]);
 
-degreeInfo["CS"].addReq("CS401",["CS331"]);
+degreeInfo["CS"].addPreReq("CS401",["CS331"]);
 
-degreeInfo["CS"].addReq("CS403",["CIS355"]);
-degreeInfo["CS"].addReq("CS403",["CS401","CIS301"]);
+degreeInfo["CS"].addPreReq("CS403",["CIS355"]);
+degreeInfo["CS"].addPreReq("CS403",["CS401","CIS301"]);
 
-degreeInfo["CS"].addReq("CS411",["CS216"]);
+degreeInfo["CS"].addPreReq("CS411",["CS216"]);
 
-degreeInfo["CS"].addReq("CS421",["CIS355"]);
-degreeInfo["CS"].addReq("CS421",["CIS357"]);
-degreeInfo["CS"].addReq("CS421",["CIS401"]);
-degreeInfo["CS"].addReq("CS421",["CIS411"]);
+degreeInfo["CS"].addPreReq("CS421",["CIS355"]);
+degreeInfo["CS"].addPreReq("CS421",["CIS357"]);
+degreeInfo["CS"].addPreReq("CS421",["CIS401"]);
+degreeInfo["CS"].addPreReq("CS421",["CIS411"]);
 
-degreeInfo["CS"].addReq("CS433",["CS333"]);
+degreeInfo["CS"].addPreReq("CS433",["CS333"]);
 
-degreeInfo["CS"].addReq("CS446",["CIS331"]);
+degreeInfo["CS"].addPreReq("CS446",["CIS331"]);
 
-degreeInfo["CS"].addReq("CS451",["CS316"]);
+degreeInfo["CS"].addPreReq("CS451",["CS316"]);
 
-degreeInfo["CS"].addReq("CS471",["CS421"]);
-
-
+degreeInfo["CS"].addPreReq("CS471",["CS421"]);
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 //Check out http://www.graphdracula.net/ for a network graph that might pair well with the way data is stored
 //Use http://visjs.org/network_examples.html for a more feature rich one.
 
+var termFilter="16/WI";
+var degreeFilter="CS";
+var debug=true;
+
 //Will need a object to hold data in a usable format
 var ClassInfo = function (classData) { //Pass in the course
     this.name=classData.prefix+classData.courseNumber;      //Holds the class id CS255
@@ -25,29 +29,139 @@ var ClassInfo = function (classData) { //Pass in the course
             }
         });
         return ok;
-    }
-
-}
+    };
+};
 
 var Degree = function() {
+    this.nodes=new vis.DataSet();
+    this.edges=new vis.DataSet();
+
     this.required=[];
     this.electives=[];
+    this.electiveCredit=0;
+
     this.reqs={};
     this.concurrent={};
-    this.electiveCredit=0;
-    this.addReq = function addReq(course, arrReqs) {
-        this.reqs[course]=this.reqs[course] || []; //Init it if we haven't already
-        this.reqs[course].push(new prereq(arrReqs));
-    }
-    this.addConcurrent = function addReq(course, arrReqs) {
-        this.concurrent[course]=this.reqs[course] || []; //Init it if we haven't already
-        this.concurrent[course].push(new prereq(arrReqs));
-    }
-}
+    this.priorityList={};
 
-var prereq = function (arr) {
-    this.rec = arr; //this array will hold equivalent classes. If you needed either MATH 120a or MATH 120b then it would hold ["MATH120a","MATH120b"]
-}
+    this.addRequired=function addRequired(classNames) {
+        this.required.push(classNames);
+        var that=this;
+        $.each(classNames,function(index,value) {
+            that.nodes.add([{
+                id: value,
+                label: value
+            }]);
+        });
+    };
+
+
+    this.addElective=function addElective(classNames) {
+        this.electives.push(classNames);
+    };
+
+    this.addPreReq = function addReq(course, arrReqs) {
+        this.reqs[course]=this.reqs[course] || []; //Init it if we haven't already
+        this.reqs[course].push(arrReqs);
+        var that=this;
+        $.each(arrReqs,function(index,value) {
+            that.edges.add([{
+                from: value,
+                to: course,
+                arrows:'to'
+            }]);
+        });
+    };
+
+    this.addConcurrent = function addConcurrent(course, arrReqs) {
+        this.concurrent[course]=this.concurrent[course] || []; //Init it if we haven't already
+        this.concurrent[course].push(arrReqs);
+    };
+
+    this.getGraphData = function getGraphData() {
+        return {
+            nodes: this.nodes,
+            edges: this.edges
+        };
+    };
+
+    this.getClassList = function getClassList() {
+        var cl = _.flatten(_.union(_.flatten(this.required), _.flatten(this.electives),
+            _.keys(this.reqs),_.flatten(_.map(this.reqs, _.values)),
+            _.keys(this.concurrent),_.flatten(_.map(this.concurrent, _.values))
+        ));
+        return cl;
+    };
+
+    this.getDeptList = function getDeptList() {
+        var re = /[A-Z]*/;
+        var strs = this.getClassList();
+        var m;
+        var deptList=[];
+        $.each(strs,function(index,str) {
+            if ((m = re.exec(str)) !== null) {
+                if (m.index === re.lastIndex) {
+                    re.lastIndex++;
+                }
+                deptList.push(m[0]);
+            }
+        });
+        return _.uniq(deptList);
+    };
+
+    this.calcPriorityAll = function calcPriorityAll() {
+        //List of everything that is a prereq
+        var isPreReq  = _.union(_.keys(this.reqs), _.keys(this.concurrent));
+        var cl = this.getClassList();
+        //Init prioritylist
+        var that=this;
+
+        $.each(cl,function(index,value) {
+            that.priorityList[value] = 0;
+        });
+
+        $.each(cl,function(indx,node) {
+            that.updatePriority(node);
+        });
+        /*
+        //Loop though each required and calculate the priority{
+        $.each(cl,function(indx,value) {
+            if (that.priorityList[value] === 0) {
+                //We need to calc our priority
+                that.priorityList[value]=that.calcPriority(value);
+            }
+        });
+        */
+    };
+
+    this.updatePriority = function updatePriority(node) {
+        var that=this;
+        if (typeof that.reqs[node] === 'undefined') { //I have no prereqs so I am a value 1 node
+            return true;
+        }
+        $.each(that.reqs[node],function (index,reqsArr) {
+            $.each(reqsArr,function (index,reqClass) {
+                that.priorityList[reqClass] += 1;
+                that.updatePriority(reqClass);
+            });
+        }) ;
+    };
+
+    this.canTake = function (node,classes) { // node is the class you want to take, classes is what you have already taken in an array
+        var ok = true;
+        if (typeof this.reqs[node] === 'undefined') //We have no prereqs
+            return true;
+        $.each(this.reqs[node],function(key,value) {
+            //Value will now be an array of prereqs, we need one of each. If this fails then return false, we don't have
+            //what we need.
+            if (_.intersection(classes,value).length===0) { //We are missing a req
+                ok=false;
+                return false; //Breaks out of our each loop
+            }
+        });
+        return ok;
+    };
+};
 
 //Array of promises to know when both the data has loaded and that the dom is ready.
 var siteReady=[];
@@ -59,17 +173,13 @@ var courses={};
 var classList = {};
 //Required classes
 var degreeInfo={};
-//Fetch the data for CS. This will run the CS.js file
-//This way we have all the degree data for CS in one file and if it ever becomes
-//available to parse instead of manually update we can just edit that file to return the data
-//in the format needed.
-siteReady.push($.getScript("./js/dal/CS.js"));
+
 //Wait until the dom is read.
 siteReady.push(domReady.promise());
 
 var app=function () {
 
-}
+};
 
 $(document).ready(function() {domReady.resolve();});
 //start our main app. This should get the page ready and then call main() once everything is setup
@@ -78,11 +188,25 @@ init();
 //Our init function should make sure all the data is ready and the dom is loaded
 function init()
 {
-    siteReady.push(getData().done(function () {
-        //Set our button from loading to check
-        //$("#runButton").text("Run");
-    }));
-    $.when.apply(null,siteReady).then(main);
+    //Fetch the data for CS. This will run the CS.js file
+    //This way we have all the degree data for CS in one file and if it ever becomes
+    //available to parse instead of manually update we can just edit that file to return the data
+    //in the format needed.
+    $.getScript("./js/dal/"+degreeFilter+".js").done(function() {
+        degreeInfo[degreeFilter].calcPriorityAll();
+        siteReady.push(getData().done(function () {
+            //Set our button from loading to check
+            //$("#runButton").text("Run");
+        }));
+        $.when.apply(null, siteReady).then(main);
+    }).fail(function(){
+        if(arguments[0].readyState==0){
+            console.error("Script failed to load");
+        }else{
+            //script loaded but failed to parse
+            console.error(arguments[2].toString());
+        }
+    });
 }
 //Our data is loaded and our dom is loaded. Process the data and bind our events.
 function main() {
@@ -95,6 +219,8 @@ function main() {
 
         //Loop though each course in CS and make our psudo
         $.each(dept,function (i,classData) {
+            if (classData.term !== termFilter)
+                return true; //Same as continue
             var classInfo = new ClassInfo(classData);
             //Work around because !== " " was always true for some reason
             if ($.trim(classData.prerequisites) != '') {
@@ -102,19 +228,85 @@ function main() {
                     if (_.has(degreeInfo[deptName].reqs, classInfo.name)) {
                         classInfo.prereqs = degreeInfo[deptName].reqs[classInfo.name];
                     }
-                    console.log(classData);
                 }
             }
             //Store our class
-            classList[classInfo.name]=classInfo;
+            classList[classInfo.name]=classList[classInfo.name]||[];
+            classList[classInfo.name].push(classInfo);
         });
     })
-    console.log(classList);
-    /* example of checking for prereq
-    console.log(classList["CS116"].hasPrereqs(["MATH103","CS105"]));
-    console.log(classList["CS116"].hasPrereqs(["CS105"]));
-    console.log(classList["CS101"].hasPrereqs(["CS105"]));
+    //console.log(degreeInfo[degreeFilter].getGraphData());
+    debugPrint("Can I take 116? " ,degreeInfo['CS'].canTake("CS116",["MATH103","CS105"]));
+     //example of checking for prereq
+    /*
+    console.log(classList["CS116"][0].hasPrereqs(["MATH103","CS105"]));
+    console.log(classList["CS116"][0].hasPrereqs(["CS105"]));
+    console.log(classList["CS101"][0].hasPrereqs(["CS105"]));
     */
+
+    //Fill in our checkbox list
+    var checkTemplate=_.template('<label class="checkbox-inline no_indent"><input type="checkbox" class="checkbox-classList" value="<%=name%>"> <%=name%></label>');
+    var sortedClassList = _.sortBy(degreeInfo[degreeFilter].getClassList(),function (name) {return name});
+    $.each(sortedClassList,function (index,value) {
+        var checkBoxHTML = checkTemplate({name: value});
+        $("#classList").append(checkBoxHTML);
+    });
+
+    //Draw our graph
+    var container = $("#courseChart")[0];
+    var options = {
+        layout: {
+            hierarchical: {
+                direction: 'LR',
+                sortMethod: 'directed'
+            }
+        }
+    };
+    var network = new vis.Network(container, degreeInfo[degreeFilter].getGraphData(), options);
+    console.log(degreeInfo["CS"]);
+}
+
+function calcSchedule() {
+    //Make an array of what we have taken
+    var classesTaken = $('.checkbox-classList:checked').map(function () {
+        return this.value;
+    }).get();
+    //Sort it by name
+    classesTaken=_.sortBy(classesTaken,function (name) {return name});
+    //Sort classes by priority
+    debugPrint("Priority list",degreeInfo[degreeFilter].priorityList);
+    var classPriority = _.map(degreeInfo[degreeFilter].priorityList, function(val,key) {
+        return {name:key,priority:val};
+    });
+    classPriority= _.sortBy(classPriority,'priority').reverse();
+
+    //Select classes until I hit max credit. Classes should not be selected if it conflicts with an already scheduled class
+    //or I have already taken it
+    var toTake=[];
+    var creditsTaken=0;
+    debugPrint("classes taken so far",classesTaken);
+    $.each(classPriority,function(index,value) {
+        if (typeof classList[value.name] === 'undefined' ) {
+            debugPrint("Class not offered "+value.name);
+            return true; //Class isn't offered so we skip it.
+        }
+        var classData = classList[value.name];
+        if (_.indexOf(classesTaken,classData[0].name,true) ===-1) { //select it only if we haven't already taken it
+            debugPrint("Can I take " + value.name ,degreeInfo['CS'].canTake(value.name,classesTaken));
+            if (degreeInfo[degreeFilter].canTake(value.name,classesTaken)) {
+                debugPrint("Taking ", classData[0]);
+                toTake.push(value.name);
+                creditsTaken += parseInt(classData[0].credits);
+                debugPrint("Credits taken ", creditsTaken);
+                if (creditsTaken >= 16) {
+                    debugPrint("Taken all we can");
+                    return false;
+                }
+            }
+        }
+    });
+    debugPrint("Take: ",toTake);
+    $("#classToTake").text(toTake);
 }
 
 //Pull down our data one department at a time
@@ -123,7 +315,9 @@ function getData() {
     var myPromise = $.Deferred();
     var getPromise=[];
     //List of dept prefixes
-    var depts = ["ACCT", "ART", "BIOL", "BL", "BLS", "CBM", "CDM", "CHEM", "CIS", "CJ", "CMA", "COMM", "CS", "ECE", "ECON", "EDL", "ELP", "ENGL", "ETD", "ETM", "FIN", "FREN", "GEOG", "GER", "GS", "HIST", "HON", "HS", "HUM", "JAPN", "KINE", "LAT", "LAW", "LS", "MAS", "MATH", "ME", "MEM", "MFL", "MGT", "MKT", "MLS", "MUS", "MUSE", "NS", "NURS", "OT", "PHE", "PHIL", "PHYS", "PLSH", "PS", "PSYC", "RFS", "RPW", "RS", "SOC", "SPAN", "SW", "TCFL", "TE", "TEMS", "THEA", "YS"];
+    //var depts = ["ACCT", "ART", "BIOL", "BL", "BLS", "CBM", "CDM", "CHEM", "CIS", "CJ", "CMA", "COMM", "CS", "ECE", "ECON", "EDL", "ELP", "ENGL", "ETD", "ETM", "FIN", "FREN", "GEOG", "GER", "GS", "HIST", "HON", "HS", "HUM", "JAPN", "KINE", "LAT", "LAW", "LS", "MAS", "MATH", "ME", "MEM", "MFL", "MGT", "MKT", "MLS", "MUS", "MUSE", "NS", "NURS", "OT", "PHE", "PHIL", "PHYS", "PLSH", "PS", "PSYC", "RFS", "RPW", "RS", "SOC", "SPAN", "SW", "TCFL", "TE", "TEMS", "THEA", "YS"];
+    //console.log(degreeInfo["CS"].getDeptList());
+    var depts=degreeInfo[degreeFilter].getDeptList();
     $("#loading-progressbar").progressbar({
         max: depts.length,
         value: 0
@@ -141,3 +335,21 @@ function getData() {
     return myPromise.promise(); //return our promise;
 }
 
+function debugPrint (msg,obj) {
+    if (debug) {
+        var prefix="D/:";
+        if (typeof msg === 'string') {
+            if (typeof obj !== 'undefined'){
+                console.log(prefix+"=====[ " + msg + " ]=====");
+                console.log(obj);
+            }
+            else{
+                console.log(prefix+":" + msg);
+            }
+        }
+        else
+        {   console.log(prefix+":");
+            console.log(msg);
+        }
+    }
+};
